7fbc950d8607278d60a885f6458ebf2818189c2f
ToanDM <dinhmanhtoan1998@gmail.com>
2021-04-24 19:12:13 +0700

    'checkout'

---
+++ Model/ViewModel/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModel
+{
+   public class CartItem
+    {
+        public long quantity { set; get; }
+        public long Id { get; set; }
+        //public Product product { set; get; }
+    }
+}
